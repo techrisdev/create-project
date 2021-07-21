326a181086a5b5bec0b0a25401662663a1102ce6
techrisdev <techrisdev>
2021-07-21 20:02:47 +0200

    The script now creates the directory and the local git repository.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import os
+
 print("What kind of Project do you want to create?")
 print("0: Empty (default)")
 print("1: Flutter")
 kind = input("Number: ")
-print("'" + kind + "'")
 
-if kind == "":
+if not kind == '1':
     # Create an empty project by default
-    kind = 0
+    kind = '0'
 
+project_name = input("Name of your Project: ")
+
+# Create a directory in $HOME/dev/Projects
+directory = "$HOME/dev/Projects/" + project_name
+os.system("mkdir -p " + directory + "/src")
+
+# Create a local git repository
+os.system("cd " + directory + "; git init; git add .; git commit -m 'Initial Commit'")
 
